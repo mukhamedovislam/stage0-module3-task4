25dc83102f86b92ca22d648e514e4375b401ea07
mukhamedovislam <bozuy24@gmail.com>
2022-09-08 09:42:27 +0400

    First Submit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+    	char a = 'a';
+	char b = 'b';
+
+	double theFormula = (9 * a^2 - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+
     }
 }
